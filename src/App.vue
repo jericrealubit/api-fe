8e314ab4e9f487a0742d4979965de049af71700b
jeric realubit <jericrealubit@gmail.com>
2022-08-19 00:37:25 +1200

    REST

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
   <h1>123</h1>
   <ul>
     <li v-for="profile in profiles">
+    {{ profile._id }} 
       {{ profile.username }}
+      <img :src="profile.imageUrl" alt="">
+      {{profile.imageUrl}}
     </li>
   </ul>
   <button @click="insertDoc">Insert</button>
@@ -23,57 +26,67 @@
     methods: {
       insertDoc() {
         fetch(api, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            // data you intend to send as JSON to the server                  
-            "username": "fri",
-            "email": "fri@gmail.com",
-            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              // data you intend to send as JSON to the server                  
+              "username": "fri",
+              "email": "fri@gmail.com",
+              "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+            })
           })
-        })
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data)
+          })
+          .catch((err) => {
+            if (err) throw err;
+          })
       },
       getDoc() {
-        fetch(api, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            // data you intend to send as JSON to the server                  
-            "username": "fri",
-            "email": "fri@gmail.com",
-            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+        fetch(api + "62f24addf749e768f410f908", {
+            method: 'GET'
           })
-        })
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data)
+          })
+          .catch((err) => {
+            if (err) throw err;
+          })
       },
       deleteDoc() {
-        fetch(api, {
-          method: 'POST',
+        fetch(api + "62f249c8f749e768f410f907", {
+            method: 'DELETE'           
+          })
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data)
+          })
+          .catch((err) => {
+            if (err) throw err;
+          })
+        
+      },
+      updateDoc() {
+        fetch(api + "62f24addf749e768f410f908", {
+          method: 'PUT',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({
-            // data you intend to send as JSON to the server                  
-            "username": "fri",
+          body: JSON.stringify({              
+            "username": "satfri",
             "email": "fri@gmail.com",
             "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
           })
-        })
-      },
-      updateDoc() {
-        fetch(api, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            // data you intend to send as JSON to the server                  
-            "username": "fri",
-            "email": "fri@gmail.com",
-            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+          .then((response) => response.text())
+          .then((data) => {
+            console.log(data)
+          })
+          .catch((err) => {
+            if (err) throw err;
           })
         })
       }   
