8f43496f68fc387760b79b692faeacfbd7910357
jeric realubit <jericrealubit@gmail.com>
2022-08-19 10:02:53 +1200

    minor fix for the returned data for GET

--- src/App.vue
+++ src/App.vue
@@ -33,10 +33,10 @@
               'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-              // data you intend to send as JSON to the server                  
-              "username": "fri123",
-              "email": "fri@gmail.com",
-              "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+              // data you intend to send as JSON to the server (payload)
+              "username": "apple",
+              "email": "apple@gmail.com",
+              "imageUrl": "https://iili.io/gYiWOJ.jpg"            
             })
           })
           .then((response) => response.text())
@@ -71,7 +71,7 @@
         fetch(api + this.id, {
             method: 'GET'
           })
-          .then((response) => response.text())
+          .then((response) => response.json())
           .then((data) => {
             console.log(data)
           })
