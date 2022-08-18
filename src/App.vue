1a3a16e9fa3de5763df71613fa50765959be192c
jeric realubit <jericrealubit@gmail.com>
2022-08-18 17:08:12 +1200

    added rest buttons

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,9 @@
       {{ profile.username }}
     </li>
   </ul>
+  <button @click="insertDoc">Insert</button>
+  <button @click="getDoc">Get</button>
+  <button @click="deleteDoc">Delete</button>
   <button @click="updateDoc">Update</button>
 </template>
 
@@ -18,6 +21,48 @@
       }
     },
     methods: {
+      insertDoc() {
+        fetch(api, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            // data you intend to send as JSON to the server                  
+            "username": "fri",
+            "email": "fri@gmail.com",
+            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+          })
+        })
+      },
+      getDoc() {
+        fetch(api, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            // data you intend to send as JSON to the server                  
+            "username": "fri",
+            "email": "fri@gmail.com",
+            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+          })
+        })
+      },
+      deleteDoc() {
+        fetch(api, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            // data you intend to send as JSON to the server                  
+            "username": "fri",
+            "email": "fri@gmail.com",
+            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+          })
+        })
+      },
       updateDoc() {
         fetch(api, {
           method: 'POST',
@@ -31,7 +76,7 @@
             "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
           })
         })
-      }      
+      }   
     },
     mounted() {      
       fetch(api)
