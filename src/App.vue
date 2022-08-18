eaa563fc9765e084fec3dd6e3dd5e522cd3e71a3
jeric realubit <jericrealubit@gmail.com>
2022-08-18 17:01:23 +1200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <h1>123</h1>
+  <ul>
+    <li v-for="profile in profiles">
+      {{ profile.username }}
+    </li>
+  </ul>
+  <button @click="updateDoc">Update</button>
+</template>
+
+<script>
+  const api = "https://rest-api-express-mongodb.netlify.app/.netlify/functions/api/" 
+  export default {
+    data() {
+      return {
+        profiles: []
+      }
+    },
+    methods: {
+      updateDoc() {
+        fetch(api, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            // data you intend to send as JSON to the server                  
+            "username": "fri",
+            "email": "fri@gmail.com",
+            "imageUrl": "https://drive.google.com/file/d/1hZfS5P7133Z64ylWcyPTs7PebV_EO_om/view?usp=sharing"            
+          })
+        })
+      }      
+    },
+    mounted() {      
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          this.profiles = data
+        })
+        .catch((err) => {
+          if (err) throw err;
+        })
+    }
+  }
+</script>
+
